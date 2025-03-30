3c886d8f5df0a74c47f76df3acac1171772b15e1
Oleg Novikov <o.novikov@ymail.com>
2025-03-30 23:11:29 +0100

    feat: add API GW service

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,41 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  Module,
+  MiddlewareConsumer,
+  RequestMethod,
+  NestModule,
+} from '@nestjs/common';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import * as process from 'node:process';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+const services = [
+  {
+    prefix: '/inventory',
+    target: process.env.INVENTORY_SERVICE_URL || 'http://localhost:3011',
+  },
+  {
+    prefix: '/catalog',
+    target: process.env.CATALOG_SERVICE_URL || 'http://localhost:3012',
+  },
+  {
+    prefix: '/auth',
+    target: process.env.AUTH_SERVICE_URL || 'http://localhost:3013',
+  },
+];
+
+// Реализация простого API GW на основе middleware http proxy
+@Module({})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    services.forEach(({ prefix, target }) => {
+      consumer
+        .apply(
+          createProxyMiddleware({
+            target,
+            changeOrigin: true,
+            pathRewrite: { [`^${prefix}`]: '' },
+          }),
+        )
+        .forRoutes({ path: `${prefix}/*path`, method: RequestMethod.ALL });
+    });
+  }
+}
